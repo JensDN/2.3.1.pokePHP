50c26c785eabc2b6627151744bf3b6e849d7f4fb
JensDN <57954369+JensDN@users.noreply.github.com>
2020-02-06 15:33:44 +0100

    Thirds Steps

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ERROR);
-
-$pokeInput = $_GET['pokemon'];
+$pokeInput = $_GET['pokemon']; // get request for an input form
 switch (isset($pokeInput)) {
     case true:
         $pokemon = makeArr(getData($pokeInput));
@@ -12,30 +11,34 @@ switch (isset($pokeInput)) {
     case false:
         $pokemon = [
     'name' => 'pls input pokemon',
-    'sprite' => '#',
+    'sprite' => './default.png',
     'moves' => '#',
-    'evolutionChain' => '#'
+    'evolutionChain' => ['firstEvo' => null, 'secondEvo' => null, 'thirdEvo' => null]
     ];
             break;
         default:
             echo "Where is that input?;";
-}
-var_dump($pokemon);
+}; // switch case  for the first when input is empty;
 function getData($input)
 {
     $baseEndpoint = 'http://pokeapi.co/api/v2/pokemon/';
     $data = file_get_contents($baseEndpoint . $input . '/');
     return json_decode($data, true);
-}
+} // function to receive data from the pokeApi
 function makeArr($data){
-    $arr = [
+    return [
         'name' => $data['name'],
         'sprite' => $data['sprites']['front_default'],
         'moves' => randomMoves($data['moves'], 4),
         'evolutionChain' => evolutionChain($data['species']['url'])
     ];
-    return $arr;
-}
+
+
+} // create an array from  all the valuable date from the inputData
+function evolutionSprite ($evoName) {
+    $data = getData($evoName);
+    return $data['sprites']['front_default'];
+} // to call the imageURl from the evolutions of the pokemon
 function evolutionChain($urlEvoChain): array
 {
     $responseForChain = file_get_contents($urlEvoChain);
@@ -44,12 +47,12 @@ function evolutionChain($urlEvoChain): array
     $responseChain = file_get_contents($evolutionChainUrl);
     $dataChain = json_decode($responseChain, true);
 
-    is_null($dataChain['chain']['species']['name'])?$firstEvolution ='No first evolution' : $firstEvolution = $dataChain['chain']['species']['name'];
-    is_null($dataChain['chain']['evolves_to'][0]['species']['name'])?$secondEvolution ='No second evolution' : $secondEvolution = $dataChain['chain']['evolves_to'][0]['species']['name'];
-    is_null($dataChain['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'])?$thirdEvolution ='No third evolution' : $thirdEvolution = $dataChain['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'];
+    is_null($dataChain['chain']['species']['name'])?$firstEvolution =['text' => 'No first evolution', 'img' => null] : $firstEvolution = ['text' => $dataChain['chain']['species']['name'], 'imgURL' =>evolutionSprite($dataChain['chain']['species']['name'])];
+    is_null($dataChain['chain']['evolves_to'][0]['species']['name'])?$secondEvolution =['text' => 'No second evolution', 'img' => null] : $secondEvolution = ['text' => $dataChain['chain']['evolves_to'][0]['species']['name'], 'imgURL' =>evolutionSprite($dataChain['chain']['evolves_to'][0]['species']['name'])];
+    is_null($dataChain['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'])?$thirdEvolution =['text' => 'No third evolution', 'img' => null] : $thirdEvolution = ['text' =>$dataChain['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'], 'imgURL' =>evolutionSprite($dataChain['chain']['evolves_to'][0]['evolves_to'][0]['species']['name'])];
 
     return ['firstEvo' => $firstEvolution, 'secondEvo' => $secondEvolution, 'thirdEvo' => $thirdEvolution];
-};
+};  //return an associate Array for the evolutions of every species
 function randomMoves(array $Moves, int $thatArrLenght): array {
     $minvalue = min($thatArrLenght, count($Moves));
     $keysArr = array_rand($Moves, $minvalue);
@@ -61,10 +64,10 @@ function randomMoves(array $Moves, int $thatArrLenght): array {
         foreach ($keysArr as $value) {
             array_push($arrayWithAllThe4Moves, $Moves[$value]['move']['name']);
         }
-
     };
     return $arrayWithAllThe4Moves;
-};
+}; //create an new shuffeld  array with a lenght provided bij a int
+
 ?>
 
 
@@ -90,11 +93,12 @@ function randomMoves(array $Moves, int $thatArrLenght): array {
     <div class="divider"></div>
     <div class="stats-display">
         <h2><?php echo $pokemon['name'] ?></h2>
-   <!--     <h3>Abilities</h3>
+         <h3>Evolution</h3>
         <ul>
-            <li>Solar-power</li>
-            <li>Blaze</li>
-        </ul>-->
+            <?php foreach ( $pokemon['evolutionChain'] as $itemEvo){
+                echo '<li>'.$itemEvo['text'].'</li>';
+            } ?>
+        </ul>
         <h3>Moves</h3>
         <ul>
             <?php foreach ($pokemon['moves'] as $itemMove){
@@ -122,14 +126,24 @@ function randomMoves(array $Moves, int $thatArrLenght): array {
     </div>
 
     <div class="bottom-modes">
+        <a href=<?php echo 'http://pokephp.localhost/?pokemon='.$pokemon['evolutionChain']['firstEvo']['text']?>>
+        <button class="level-button" type="button">
+            <img src=<?php echo $pokemon['evolutionChain']['firstEvo']['imgURL']?> alt=<?php echo $pokemon['evolutionChain']['firstEvo']['text']?> >
+        </button>
+        </a>
+        <a href=<?php echo 'http://pokephp.localhost/?pokemon='.$pokemon['evolutionChain']['secondEvo']['text']?>>
+            <button class="level-button" type="button">
+                    <img src=<?php echo $pokemon['evolutionChain']['secondEvo']['imgURL']?> alt=<?php echo $pokemon['evolutionChain']['secondEvo']['text']?> >
+            </button>
+        </a>
+        <a href=<?php echo 'http://pokephp.localhost/?pokemon='.$pokemon['evolutionChain']['thirdEvo']['text']?>>
+            <button class="level-button" type="button">
+                <img src=<?php echo $pokemon['evolutionChain']['thirdEvo']['imgURL']?> alt=<?php echo $pokemon['evolutionChain']['thirdEvo']['text']?> >
+            </button>
+        </a>
 
-        <button class="level-button"></button>
-        <button class="level-button"></button>
-        <button class="level-button"></button>
-        <button class="level-button"></button>
 
-        <button class="pokedex-mode black-button">Pokedex</button>
-        <button class="game-mode black-button">Game</button>
+
 
     </div>
 
